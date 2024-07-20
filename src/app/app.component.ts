785f3bf50ecdc006bfe845cd2123f35b3bc83fe3
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-07-21 01:22:26 +0300

    implement users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
   title = 'task-app';
+
+  users = DUMMY_USERS;
+  selectedUserId = 'u1'
+
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id:string){
+    this.selectedUserId = id;
+  }
 }
