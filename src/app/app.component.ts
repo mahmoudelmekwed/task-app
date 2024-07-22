57f6bd4990e3e29b7d2ee122f6237938d3e65269
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-07-22 04:48:58 +0300

    build task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   title = 'task-app';
 
   users = DUMMY_USERS;
-  selectedUserId = 'u1'
+  selectedUserId?: string;
 
   get selectedUser(){
     return this.users.find((user) => user.id === this.selectedUserId);
